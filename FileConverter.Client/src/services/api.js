4ae5e9b05629d60c84e876f2c1d4a4e1365010d0
Ali Buğra Kodalak <alibugrakodalak1@gmail.com>
2025-12-03 11:17:00 +0300

    API adresi canlı sunucuya (Render) yönlendirildi

--- FileConverter.Client/src/services/api.js
+++ FileConverter.Client/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Backend adresimiz (Visual Studio'da gördüğün portu buraya yazman gerekecek)
 // Portu 5000'den 5050'ye çekiyoruz
 const apiClient = axios.create({
-  baseURL: "http://localhost:5050/api", // <-- DİKKAT: 5050 OLDU
+  baseURL: "https://file-converter-app-sr99.onrender.com/api", // <-- DİKKAT: 5050 OLDU
   headers: {
     "Content-Type": "multipart/form-data",
   },
